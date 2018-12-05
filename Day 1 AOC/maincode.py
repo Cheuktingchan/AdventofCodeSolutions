6da6f88484b7072aca1169c690a273cae233bbc6
Varraca <45636062+Varraca@users.noreply.github.com>
2018-12-05 22:05:48 +0000

    Update maincode.py

--- Day 1 AOC/maincode.py
+++ Day 1 AOC/maincode.py
@@ -1,6 +1,21 @@
+########### PART 1 ##############
+
 nums = open("numbers.txt", "r")
 n = nums.read()
 nlist = n.split("\n")
 anslist = list(map(int,nlist))
 ans = sum(anslist)
 print(ans)
+
+######### PART 2 ################
+
+ptwodic = {}
+ptwodic["0"] = anslist[0]
+for i in range(len(anslist)):
+    if i == 0:
+        pass
+    else:
+        ptwodic[str(i)] = ptwodic[str(i-1)] + anslist[i]
+
+ptwolist = list(ptwodic.values())
+
