33f5208551fd228d9d99bb184fff4a340339afb1
Varraca <45636062+Varraca@users.noreply.github.com>
2018-12-07 21:46:18 +0000

    Update maincode.py

--- Day 2 AOC/maincode.py
+++ Day 2 AOC/maincode.py
@@ -22,3 +22,14 @@ for boxid in boxes:
 print(len(twotimes)*len(threetimes))
 
 #############PART 2#######################
+
+for i,boxid1 in enumerate(boxes):
+    for boxid2 in boxes[i + 1:]:
+        letsdiff = 0
+        for a,char in enumerate(boxid1):
+            char2 = boxid2[a]
+            if char != char2:
+                letsdiff += 1
+        if letsdiff == 1:
+            print(boxid1,boxid2)
+            break
