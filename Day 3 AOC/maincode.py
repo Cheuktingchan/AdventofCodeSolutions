ec43ca19e1dad507a9dd4a5e2af77c194db37192
Varraca <45636062+Varraca@users.noreply.github.com>
2018-12-08 18:42:51 +0000

    Add files via upload

--- Day 3 AOC/maincode.py
+++ Day 3 AOC/maincode.py
@@ -5,13 +5,11 @@ elves = open("claims.txt","r")
 elv = elves.read()
 claims = elv.split("\n")
 
-def get_cords_and_dims(claim):
+
+for claim in claims:
     clem,clem,coord,dimen = claim.split()
     cordx,cordy = map(int,coord[:-1].split(","))
     wid,hei = map(int,dimen.split("x"))
-    return cordx,cordy,wid,hei
-for claim in claims:
-    cordx,cordy,wid,hei = get_cords_and_dims(claim)
     for row in range(hei):
         for col in range(wid):
             grid[cordy + row][cordx + col] += 1
