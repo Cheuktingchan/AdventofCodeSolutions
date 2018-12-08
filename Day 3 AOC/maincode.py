9bece23f42ed5fe12464ff00ccb80561fb588c2b
Varraca <45636062+Varraca@users.noreply.github.com>
2018-12-08 14:12:52 +0000

    Update maincode.py

--- Day 3 AOC/maincode.py
+++ Day 3 AOC/maincode.py
@@ -14,6 +14,35 @@ size = []
 
 for i in claims:
     claimno.append(i.split("@")[0])
-print(claimno)
+    coords.append(i.split(" ")[2])
+    size.append(i.split(" ")[3])
+
+claimnor = []
+for claimn in claimno:
+    claimnor.append(claimn[1:])
+claimno = list(map(int,claimnor))
+
+chordx = []
+coordy = []
+
+for coord in coords:
+    chordx.append(coord.split(",")[0])
+    coordy.append(coord.split(",")[1])
+    
+chordy = []
+
+for cody in coordy:
+    chordy.append(cody.split(":")[0])
+coordy = list(map(int,chordy))
+coordx = list(map(int,chordx))
+
+sizex = [] 
+sizey = []
+for sides in size:
+    sizex.append(sides.split("x")[0])
+    sizey.append(sides.split("x")[1])
+
+sidex = list(map(int,sizex))
+sidey = list(map(int,sizey))
             
     
