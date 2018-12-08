d825e2f1cfb6de957898b71781af6fa0fd2ca7ed
Varraca <45636062+Varraca@users.noreply.github.com>
2018-12-08 18:14:41 +0000

    Add files via upload

--- Day 3 AOC/maincode.py
+++ Day 3 AOC/maincode.py
@@ -1,67 +1,25 @@
-import itertools
-grid = {}
-for i in range(1000):
-    grid[str(i)] = []
-    for it in range(1001):
-        grid.setdefault(str(it),[]).append(0)
+grid = [[0]*1000 for i in range(1000)]
+    
 
 elves = open("claims.txt","r")
 elv = elves.read()
 claims = elv.split("\n")
 
-claimno = []
-coords = []
-size = []
-
-for i in claims:
-    claimno.append(i.split("@")[0])
-    coords.append(i.split(" ")[2])
-    size.append(i.split(" ")[3])
-
-claimnor = []
-for claimn in claimno:
-    claimnor.append(claimn[1:])
-claimno = list(map(int,claimnor))
-
-chordx = []
-coordy = []
-
-for coord in coords:
-    chordx.append(coord.split(",")[0])
-    coordy.append(coord.split(",")[1])
-    
-chordy = []
-
-for cody in coordy:
-    chordy.append(cody.split(":")[0])
-coordy = list(map(int,chordy))
-coordx = list(map(int,chordx))
-
-sizex = [] 
-sizey = []
-for sides in size:
-    sizex.append(sides.split("x")[0])
-    sizey.append(sides.split("x")[1])
-
-sidex = list(map(int,sizex))
-sidey = list(map(int,sizey))
-    
-for i,cordy in enumerate(coordy):
-    cordx = coordx[i]
-    sizex = sidex[i]
-    sizey = sidey[i]
-    for a in range(sizex):
-        for b in range(sizey):
-            try:
-                grid[str(cordy + b)][cordx + a] += 1
-            except IndexError:
-                continue
-
-allnums = list(grid.values())
-flat = itertools.chain.from_iterable(allnums)
+def get_cords_and_dims(claim):
+    clem,clem,coord,dimen = claim.split()
+    cordx,cordy = map(int,coord[:-1].split(","))
+    wid,hei = map(int,dimen.split("x"))
+    return cordx,cordy,wid,hei
+for claim in claims:
+    cordx,cordy,wid,hei = get_cords_and_dims(claim)
+    for row in range(hei):
+        for col in range(wid):
+            grid[cordy + row][cordx + col] += 1
 
 overlap = 0
-for a in flat:
-    if a > 1:
-        overlap += 1
+
+for f in grid:
+    for inch in f:
+        if int(inch) >= 2:
+            overlap += 1
 print(overlap)
