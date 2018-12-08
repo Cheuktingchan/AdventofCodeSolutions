331bdfbb8c9a2a5074f7baa88a8bb7da947717cc
Varraca <45636062+Varraca@users.noreply.github.com>
2018-12-08 19:44:41 +0000

    Add files via upload

--- Day 3 AOC/maincode.py
+++ Day 3 AOC/maincode.py
@@ -1,10 +1,10 @@
+###############PART 1#############################
+
 grid = [[0]*1000 for i in range(1000)]
-    
 
 elves = open("claims.txt","r")
 elv = elves.read()
 claims = elv.split("\n")
-
 
 for claim in claims:
     clem,clem,coord,dimen = claim.split()
@@ -20,4 +20,20 @@ for f in grid:
     for inch in f:
         if int(inch) >= 2:
             overlap += 1
+            
 print(overlap)
+
+########################PART 2 ######################
+
+for claim in claims:
+    clem,clem,coord,dimen = claim.split()
+    cordx,cordy = map(int,coord[:-1].split(","))
+    wid,hei = map(int,dimen.split("x"))
+    checker = []
+    for row in range(hei):
+        for col in range(wid):
+            grid[cordy + row][cordx + col] += 1
+            checker.append(grid[cordy + row][cordx + col])
+    if all(elem == 2 for elem in checker):
+        print(cordx,cordy)
+            
