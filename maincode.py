b866771817e895ffeaae6a27113fb529a7ddb693
Varraca <45636062+Varraca@users.noreply.github.com>
2018-12-10 00:17:31 +0000

    Add files via upload

---
+++ maincode.py
@@ -0,0 +1,24 @@
+opener = open("polymer.txt","r")
+polymers = opener.read()
+polymer = list(polymers)
+t = 0
+for i in range(1000):
+    try:
+        if polymer[t].isupper():
+            if polymer[t + 1] == polymer[t].lower():
+                polymer.pop(t)
+                polymer.pop(t)
+                t = 0
+            else:
+                t += 1
+        elif polymer[t].islower():
+            if polymer[t + 1] == polymer[t].upper():
+                  polymer.pop(t)
+                  polymer.pop(t)
+                  t = 0
+            else:
+                t += 1
+    except IndexError:
+        pass
+
+print(len(polymer))
