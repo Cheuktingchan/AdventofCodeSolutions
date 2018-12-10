70e5207778ce8fead39092b1e6d9da7e9984be24
Varraca <45636062+Varraca@users.noreply.github.com>
2018-12-10 07:33:36 +0000

    Add files via upload

--- Day 5 AOC/maincode.py
+++ Day 5 AOC/maincode.py
@@ -1,27 +1,24 @@
 opener = open("polymer.txt","r")
 polymers = opener.read()
 polymer = list(polymers)
-print(polymer)
 t = 0
-while True:
+for i in range(500000000):
     try:
         if polymer[t].isupper():
             if polymer[t + 1] == polymer[t].lower():
                 polymer.pop(t)
-                polymer.pop(int(t + 1))
+                polymer.pop(t)
                 t = 0
-                print(polymer)
             else:
                 t += 1
         elif polymer[t].islower():
             if polymer[t + 1] == polymer[t].upper():
                   polymer.pop(t)
-                  polymer.pop(int(t + 1))
+                  polymer.pop(t)
                   t = 0
-                  print(polymer)
-                else:
+            else:
                 t += 1
     except IndexError:
-        break
+        pass
 
-print(polymer)
+print(len(polymer))
